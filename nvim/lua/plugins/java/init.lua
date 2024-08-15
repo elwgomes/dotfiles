4b7b9139e184a977ccdf11def12e1e7ca1233190
Leonardo <elwgomes@icloud.com>
2024-08-15 10:05:15 -0300

    updt

--- nvim/lua/plugins/java/init.lua
+++ nvim/lua/plugins/java/init.lua
@@ -1,70 +1,46 @@
 return {
-  "nvim-java/nvim-java",
-  config = false,
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          jdtls = {
-            -- your jdtls configuration goes here
-          },
+    "nvim-java/nvim-java",
+    config = false,
+    dependencies = {
+        {
+            "neovim/nvim-lspconfig",
+            opts = {
+                servers = {
+                    jdtls = {
+                        settings = {
+                            java = {
+                                inlayHints = {
+                                    parameterNames = {
+                                        enabled = "none", -- Desativa os inlay hints de nomes de parâmetros
+                                    },
+                                    variableTypes = {
+                                        enabled = false, -- Desativa os inlay hints de tipos de variáveis
+                                    },
+                                    parameterTypes = {
+                                        enabled = false, -- Desativa os inlay hints de tipos de parâmetros
+                                    },
+                                    propertyNames = {
+                                        enabled = false, -- Desativa os inlay hints de nomes de propriedades
+                                    },
+                                    returnTypes = {
+                                        enabled = false, -- Desativa os inlay hints de tipos de retorno
+                                    },
+                                    enumMemberValues = {
+                                        enabled = false, -- Desativa os inlay hints de valores de membros de enumeração
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                setup = {
+                    jdtls = function()
+                        require("java").setup({
+                            -- your nvim-java configuration goes here
+                        })
+                    end,
+                },
+            },
         },
-        setup = {
-          jdtls = function()
-            require("java").setup({
-              -- your nvim-java configuration goes here
-            })
-          end,
-        },
-      },
     },
-  },
 }
-
--- return {
---     "nvim-java/nvim-java",
---     config = false,
---     dependencies = {
---         {
---             "neovim/nvim-lspconfig",
---             opts = {
---                 servers = {
---                     jdtls = {
---                         settings = {
---                             java = {
---                                 inlayHints = {
---                                     parameterNames = {
---                                         enabled = "none", -- Desativa os inlay hints de nomes de parâmetros
---                                     },
---                                     variableTypes = {
---                                         enabled = false, -- Desativa os inlay hints de tipos de variáveis
---                                     },
---                                     parameterTypes = {
---                                         enabled = false, -- Desativa os inlay hints de tipos de parâmetros
---                                     },
---                                     propertyNames = {
---                                         enabled = false, -- Desativa os inlay hints de nomes de propriedades
---                                     },
---                                     returnTypes = {
---                                         enabled = false, -- Desativa os inlay hints de tipos de retorno
---                                     },
---                                     enumMemberValues = {
---                                         enabled = false, -- Desativa os inlay hints de valores de membros de enumeração
---                                     },
---                                 },
---                             },
---                         },
---                     },
---                 },
---                 setup = {
---                     jdtls = function()
---                         require("java").setup({
---                             -- your nvim-java configuration goes here
---                         })
---                     end,
---                 },
---             },
---         },
---     },
--- }
